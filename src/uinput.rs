fa2f46be03f5cffbfd6bf20bb056feeb68bbcc57
Valink16 <valuthyk@gmail.com>
2019-03-30 13:46:17 +0300

    Trying to fix some problems

--- src/uinput.rs
+++ src/uinput.rs
@@ -26,11 +26,10 @@ pub fn choose_between(message: &str, o1: &str, o2: &str) -> bool {
 pub fn get_address(mode: OperatingMode) -> net::SocketAddr {
     // Gets an address to bind to depending on OperatingMode
     if let OperatingMode::Send = mode { // You only need to know the port to open when sending
-        return net::SocketAddr::new (
-            net::IpAddr::V4(net::Ipv4Addr::new(127, 0, 0, 1)), // we'll bind our listener to loopback
-            //get_port("Enter the port you want to open (preferred: 49152-65535): ")
+        return net::SocketAddr::V4(net::SocketAddrV4::new(
+            net::Ipv4Addr::new(127, 0, 0, 1),
             50000
-        );
+        ));
     } else { // You need both the sender's IP and port when receiving
         return net::SocketAddr::new (
             get_ip("Enter sender's IP address(IPv4 or IPv6): "),
